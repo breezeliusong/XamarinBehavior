6b43cbf66ed688676ccf9c4add271c4ff4482662
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-11-22 15:57:58 +0800

    Add project files.

---
+++ XamarinBehavior/XamarinBehavior/CustomBehavior .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinBehavior
+{
+    public class CustomBehavior:Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryChanged;
+            base.OnAttachedTo(bindable);
+            //perform setup
+        }
+
+        private void OnEntryChanged(object sender, TextChangedEventArgs e)
+        {
+            double result;
+            bool isValid = double.TryParse(e.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Green : Color.Red;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryChanged;
+            base.OnDetachingFrom(bindable);
+            //perform clean up
+        }
+    }
+}
---
+++ XamarinBehavior/XamarinBehavior/CustomBehavior1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinBehavior
+{
+    public class CustomBehavior1:Behavior<Entry>
+    {
+        public static readonly BindableProperty AttachBehaviorProperty =
+            BindableProperty.CreateAttached("AttachBehavior", 
+                typeof(bool), typeof(CustomBehavior1), false, propertyChanged: OnAttachBehaviorChanged);
+
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+
+        public static void SetAttachBehavior(BindableObject view,bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+        static void OnAttachBehaviorChanged(BindableObject view,object oldValue,object newValue)
+        {
+            var entry = view as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.Behaviors.Add(new CustomBehavior1());
+            }else
+            {
+                var toRemove = entry.Behaviors.FirstOrDefault(b => b is CustomBehavior1);
+                if (toRemove != null)
+                {
+                    entry.Behaviors.Remove(toRemove);
+                }
+            }
+        }
+    }
+}
